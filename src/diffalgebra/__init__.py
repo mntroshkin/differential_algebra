589a991792237062595f7982e6d85efb4a36fca9
mntroshkin <tr2mikh@gmail.com>
2026-07-04 20:30:17 +0300

    changed name for KdV built-in

--- src/diffalgebra/__init__.py
+++ src/diffalgebra/__init__.py
@@ -2,10 +2,10 @@ from .constant_ring import ConstantRing, QQ, ConstantPolynomial
 from .diff_ring import DifferentialRing, DifferentialPolynomial, total_derivative, partial_derivative
 from .ring_morphism import RingMorphism, DiffRingMorphism
 from .evolution_operator import EvolutionOperator
-from .kdv import KdV_flow
+from .kdv import KdV_hierarchy
 
 __all__ = ["QQ", "ConstantRing", "DifferentialRing", 
            "ConstantPolynomial", "DifferentialPolynomial", 
            "total_derivative", "partial_derivative",
            "RingMorphism", "DiffRingMorphism", "EvolutionOperator",
-           "KdV_flow"]
+           "KdV_hierarchy"]
--- src/diffalgebra/kdv.py
+++ src/diffalgebra/kdv.py
@@ -1,10 +1,10 @@
 from .diff_ring import FuncGenerator, DifferentialPolynomial
 
-def KdV_flow(u: FuncGenerator, order: int = 1) -> DifferentialPolynomial:
+def KdV_hierarchy(variable: FuncGenerator, order: int = 1) -> DifferentialPolynomial:
     if order < 0:
         raise ValueError
-    K = u[1]
+    K = variable[1]
     for i in range(order):
         L = K.integral()
-        K = L.diff(3) + 4 * u * L.diff() + 2 * u[1] * L
+        K = L.diff(3) + 4 * variable * L.diff() + 2 * variable[1] * L
     return K
